e7cdfe62cd9ca5de0e7cd6a7fbe4228106ba343c
Kipras Klimkevičius <kipras.klim@gmail.com>
2026-03-11 14:28:40 +0100

    instrument eq highpass tweaks

--- Assets/Scripts/Sound/InstrumentEQ.cs
+++ Assets/Scripts/Sound/InstrumentEQ.cs
@@ -25,35 +25,43 @@ public class InstrumentEQ : IEffects
         // Normalize the scale: 0.5 scale = 0 (Dry), 1.5 scale = 1 (Full Wet)
         // for eq we do small object -> high freq
         //float normalizedWetness = Mathf.Lerp(0.3f, 4f, wetness);
-        float resonance = Mathf.Abs(Remap(wetness, 0f, 1f, 0f, 3f));
+        
+
         
 
         // Map 0-1 normalized wetness to 200z to 22000Hz (audible range)
         //_lowpassFilter.cutoffFrequency = Mathf.Lerp(200f, 22000f, wetness);
 
         // lowpass
-        if (wetness > 0) {
+        if (!_lowpassFilter.enabled && wetness > 0.9f)
+        {
+            _lowpassFilter.enabled = true;
+            _highpassFilter.enabled = true;
+        }
+        if (wetness > 1.1f) {
             if (!_lowpassFilter.enabled)
             {
                 _lowpassFilter.enabled = true;
                 _highpassFilter.enabled = false;
             }
-            wetness = Remap(wetness, 0f, 1f, 5000f, 100f);
+            float resonance = Mathf.Abs(Remap(wetness, 0.9f, 2f, 0f, 2f));
+            wetness = Remap(wetness, 0.9f, 2f, 5000f, 400f);
             _lowpassFilter.cutoffFrequency = wetness;
-            _lowpassFilter.lowpassResonanceQ = 1 + resonance;
+            //_lowpassFilter.lowpassResonanceQ = 1 + resonance;
 
             Debug.Log("eq lowpass cutoff = " + _lowpassFilter.cutoffFrequency);
 
         } else
         {
-            if (!_highpassFilter.enabled)
+            if (!_highpassFilter.enabled && wetness < 0.9f)
             {
                 _lowpassFilter.enabled = false;
                 _highpassFilter.enabled = true;
             }
-            wetness = Remap(wetness, 0f, 1f, 5000f, 15000f);
+            float resonance = Mathf.Abs(Remap(wetness, 1.1f, 0f, 0f, 5f));
+            wetness = Remap(wetness, 1.1f, 0f, 0f, 1000f);
             _highpassFilter.cutoffFrequency = wetness;
-            _highpassFilter.highpassResonanceQ = 1 + resonance;
+            //_highpassFilter.highpassResonanceQ = 1 + resonance;
 
             Debug.Log("eq highpass cutoff = " + _highpassFilter.cutoffFrequency);
         }
