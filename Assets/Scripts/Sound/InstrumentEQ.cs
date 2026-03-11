3d19ff95f1937051e8b31947a8af0f9e9e4702b2
Kipras Klimkevičius <kipras.klim@gmail.com>
2026-03-11 17:10:49 +0100

    add chorus to instrument

--- Assets/Scripts/Sound/InstrumentEQ.cs
+++ Assets/Scripts/Sound/InstrumentEQ.cs
@@ -7,6 +7,7 @@ public class InstrumentEQ : IEffects
 
     AudioLowPassFilter _lowpassFilter;
     AudioHighPassFilter _highpassFilter;
+    AudioChorusFilter _chorus;
 
     public float scaleCoefficient = 100.0f; // this is used for setting the right cutoff freq
 
@@ -17,7 +18,10 @@ public class InstrumentEQ : IEffects
         _lowpassFilter.enabled = true;
         _highpassFilter = soundObject.GetComponent<AudioHighPassFilter>();
         _highpassFilter.enabled = false;
-        
+        _chorus = soundObject.GetComponent<AudioChorusFilter>();
+        _chorus.enabled = true;
+
+
     }
 
     public override void setWet(float wetness)
@@ -25,14 +29,20 @@ public class InstrumentEQ : IEffects
         // Normalize the scale: 0.5 scale = 0 (Dry), 1.5 scale = 1 (Full Wet)
         // for eq we do small object -> high freq
         //float normalizedWetness = Mathf.Lerp(0.3f, 4f, wetness);
-        
 
-        
+
+
 
         // Map 0-1 normalized wetness to 200z to 22000Hz (audible range)
         //_lowpassFilter.cutoffFrequency = Mathf.Lerp(200f, 22000f, wetness);
 
         // lowpass
+        //var chorusWet = Mathf.Log(Mathf.Clamp(wetness, 0f, 1f) / 3.112f + 0.1f)+1;
+        var chorusWet = Mathf.Pow((Mathf.Clamp(wetness, 0f, 2f)), 2.7182818f);
+
+        Debug.Log("ChorusWet = " + chorusWet);
+        Debug.Log("wetness = " + wetness);
+        var chorusRate = Remap(chorusWet, 0f, 2f, 10f, 0.5f);
         if (!_lowpassFilter.enabled && wetness > 0.9f)
         {
             _lowpassFilter.enabled = true;
@@ -65,6 +75,9 @@ public class InstrumentEQ : IEffects
 
             Debug.Log("eq highpass cutoff = " + _highpassFilter.cutoffFrequency);
         }
+        
+        Debug.Log("Chors rate: " + chorusRate);
+        _chorus.rate = chorusRate;
     }
 
     public float Remap(float value, float from1, float to1, float from2, float to2)
