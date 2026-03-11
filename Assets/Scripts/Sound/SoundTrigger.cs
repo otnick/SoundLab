f09686803f0a45566ad9ac1d81977d3dcb7fe81c
Sam Windheim <sam@windheim.org>
2026-03-11 17:20:45 +0100

    Drums go boom boom

--- Assets/Scripts/Sound/SoundTrigger.cs
+++ Assets/Scripts/Sound/SoundTrigger.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
@@ -13,82 +14,74 @@ namespace SoundLab.Sound
         [SerializeField] private Color _activeColor   = Color.cyan;
         [SerializeField] private float _hapticAmplitude = 0.5f;
         [SerializeField] private float _rotationSpeed   = 180f;
+        [SerializeField] private InputActionReference _sustainAction;
 
-        private AudioSource _audio;
-        private Renderer    _renderer;
+        private AudioSource   _audio;
+        private Renderer      _renderer;
+        private Material      _material;
         private IXRInteractor _activeInteractor;
-        private bool        _isSustained;
+        private IXRInteractor _lastInteractor;
+        private bool          _isSustained;
 
         private void Awake()
         {
             _audio    = GetComponent<AudioSource>();
             _renderer = GetComponentInChildren<Renderer>();
+            _material = _renderer ? _renderer.material : null;
 
             _audio.playOnAwake = false;
             _audio.loop        = true;
 
-            if (_renderer) _renderer.material.color = _defaultColor;
+            if (_material) _material.color = _defaultColor;
 
             var interactable = GetComponent<XRSimpleInteractable>();
             interactable.selectEntered.AddListener(e => OnActivate(e.interactorObject));
             interactable.selectExited.AddListener(e  => OnDeactivate(e.interactorObject));
-
-            // Primary button toggle for sustain
-            interactable.activated.AddListener(_ => ToggleSustain());
         }
 
         private void Update()
         {
-            if (_activeInteractor is XRBaseInputInteractor inputInteractor || _isSustained)
+            if (_activeInteractor is XRBaseInputInteractor inputInteractor)
             {
-                if (_activeInteractor is XRBaseInputInteractor activeInput)
-                {
-                    activeInput.SendHapticImpulse(_hapticAmplitude, Time.deltaTime);
-                }
+                inputInteractor.SendHapticImpulse(_hapticAmplitude, Time.deltaTime);
                 transform.Rotate(Vector3.up, _rotationSpeed * Time.deltaTime);
+            }
+            else if (_isSustained)
+            {
+                if (!IsPrimaryHeld())
+                    StopSound();
+                else
+                    transform.Rotate(Vector3.up, _rotationSpeed * Time.deltaTime);
             }
         }
 
-        private void ToggleSustain()
+        private bool IsPrimaryHeld()
         {
-            Debug.Log("ToggleSustain called. Current state: " + _isSustained);
-            _isSustained = !_isSustained;
-            
-            if (_isSustained)
-            {
-                if (!_audio.isPlaying)
-                {
-                    _audio.clip = _clip;
-                    _audio.Play();
-                }
-                if (_renderer) _renderer.material.color = _activeColor;
-            }
-            else if (_activeInteractor == null)
-            {
-                _audio.Stop();
-                if (_renderer) _renderer.material.color = _defaultColor;
-            }
+            return _sustainAction != null && _sustainAction.action.IsPressed();
+        }
+
+        private void StopSound()
+        {
+            _isSustained = false;
+            _audio.Stop();
+            if (_material) _material.color = _defaultColor;
         }
 
         private void OnActivate(IXRInteractor interactor)
         {
             _activeInteractor = interactor;
-            if (!_audio.isPlaying)
-            {
-                _audio.clip = _clip;
-                _audio.Play();
-            }
-            if (_renderer) _renderer.material.color = _activeColor;
+            _lastInteractor   = interactor;
+            _audio.clip = _clip;
+            _audio.Play();
+            if (_material) _material.color = _activeColor;
         }
 
         private void OnDeactivate(IXRInteractor interactor)
         {
             _activeInteractor = null;
+            _isSustained      = IsPrimaryHeld();
             if (!_isSustained)
-            {
-                _audio.Stop();
-                if (_renderer) _renderer.material.color = _defaultColor;
-            }
+                StopSound();
         }
     }
 }
