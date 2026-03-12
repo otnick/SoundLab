c4e8e9075dec9ec4517908a53996a3689d0588f7
Kipras Klimkevičius <kipras.klim@gmail.com>
2026-03-12 12:26:02 +0100

    change instrumentEQ method for setwetness/ separate into multiple methods

--- Assets/Scripts/Sound/InstrumentEQ.cs
+++ Assets/Scripts/Sound/InstrumentEQ.cs
@@ -26,23 +26,24 @@ public class InstrumentEQ : IEffects
 
     public override void setWet(float wetness)
     {
-        // Normalize the scale: 0.5 scale = 0 (Dry), 1.5 scale = 1 (Full Wet)
-        // for eq we do small object -> high freq
-        //float normalizedWetness = Mathf.Lerp(0.3f, 4f, wetness);
+        SetEQ(wetness);
+        SetChorus(wetness);
+        SetVisuals(wetness);
+    }
 
+    public float Remap(float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
 
+    void SetVisuals(float wetness)
+    {
+        
+        return;
+    }
 
-
-        // Map 0-1 normalized wetness to 200z to 22000Hz (audible range)
-        //_lowpassFilter.cutoffFrequency = Mathf.Lerp(200f, 22000f, wetness);
-
-        // lowpass
-        //var chorusWet = Mathf.Log(Mathf.Clamp(wetness, 0f, 1f) / 3.112f + 0.1f)+1;
-        var chorusWet = Mathf.Pow((Mathf.Clamp(wetness, 0f, 2f)), 2.7182818f);
-
-        Debug.Log("ChorusWet = " + chorusWet);
-        Debug.Log("wetness = " + wetness);
-        var chorusRate = Remap(chorusWet, 0f, 2f, 10f, 0.5f);
+    private void SetEQ(float wetness)
+    {
         if (!_lowpassFilter.enabled && wetness > 0.9f)
         {
             _lowpassFilter.enabled = true;
@@ -75,14 +76,18 @@ public class InstrumentEQ : IEffects
 
             Debug.Log("eq highpass cutoff = " + _highpassFilter.cutoffFrequency);
         }
+    }
+
+    void SetChorus(float wetness)
+    {
+        var chorusWet = Mathf.Pow((Mathf.Clamp(wetness, 0f, 2f)), 2.7182818f);
+
+        Debug.Log("ChorusWet = " + chorusWet);
+        Debug.Log("wetness = " + wetness);
+        var chorusRate = Remap(chorusWet, 0f, 2f, 10f, 0.5f);
         
         Debug.Log("Chors rate: " + chorusRate);
         _chorus.rate = chorusRate;
-    }
-
-    public float Remap(float value, float from1, float to1, float from2, float to2)
-    {
-        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
     }
 
 }
